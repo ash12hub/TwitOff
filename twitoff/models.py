40b94a9360785c9dd80480df98f0b78073bd9201
ash12hub <ash12hub@users.noreply.github.com>
2019-10-09 07:39:23 +0530

    Added twitter.py and user.html files

--- twitoff/models.py
+++ twitoff/models.py
@@ -7,8 +7,12 @@ class User(DB.Model):
     """Twitter users that we query and store historical tweets"""
     id = DB.Column(DB.BigInteger, primary_key=True)
     username = DB.Column(DB.String(15), unique=True, nullable=False)
+    followers = DB.Column(DB.BigInteger, nullable=False)
+    # Tweets IOs are ordinal ints, so we can fetch most recent tweets
+    recent_tweet_id = DB.Column(DB.BigInteger, nullable=False)
 
 class Tweets(DB.Model):
     """Stores Tweets"""
-    id = DB.Column(DB.Integer, primary_key=True)
-    text = DB.Column(DB.Unicode(208)) 
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(208))
+    embedded = DB.Column(DB.PickleType, nullable=False)
