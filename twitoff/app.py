b414cb0b6f5b84a56fdd0ef6c664881d5f879746
ash12hub <ash12hub@users.noreply.github.com>
2019-10-10 08:59:04 +0530

    make it better

--- twitoff/app.py
+++ twitoff/app.py
@@ -2,7 +2,8 @@ from decouple import config
 from dotenv import load_dotenv
 from flask import Flask, render_template, request
 from .models import DB, User
-from .twitter import add_or_update_user
+from .twitter import add_or_update_user, update_all_users, add_default_users
+from .predict import preduct_user
 
 def create_app():
     """Create and configure an instance of the flask application"""
@@ -29,5 +30,40 @@ def create_app():
             message = 'Error adding {}: {}'.format(name, e)
             tweets = []
         return render_template('user.html', title=name, tweets=tweets, message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1 = request.values['user1']
+        user2 = request.values['user2']
+        tweet_text = request.values['tweet_text']
+
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves!'
+        else:
+            prediction = preduct_user(user1, user2, tweet_text)
+            message = '"{}" is more likely to be said by {} than by {}'.format(
+                request.values['tweet_text'],
+                user1 if prediction else user2,
+                user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction', message=message)
+
     
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='Reset database!')
+    
+    @app.route('/update')
+    def update():
+        update_all_users()
+        return render_template('base.html', users=User.query.all(),
+                               title='All Tweets Updated')
+    
+    @app.route('/add_default')
+    def add_default():
+        add_default_users()
+        return render_template('base.html', users=User.query.all(),
+                               title='Reset database!')
+
     return app
